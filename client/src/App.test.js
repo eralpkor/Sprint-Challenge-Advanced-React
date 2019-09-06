01ad0720a545b34baf4fb3dcc0df5e7487c86801
eralpkor <eralp.ny@gmail.com>
2019-09-06 14:42:37 -0400

    added test libraries

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { render } from "@testing-library/react";
-import PlayerList from "./components/PlayerList";
+import PlayerList, { getPlayers } from "./components/PlayerList";
 import PlayerCard from "./components/PlayerCard";
+import { Simulate } from "react-dom/test-utils";
 
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it("renders without crashing", async () => {
+  await render(<App />);
+});
 
 test("should have text players", async () => {
-  const { findByText } = await render(<PlayerCard />);
-  findByText(/Ale/i);
+  const { findByText } = await render(<App />);
+  findByText(/Alex/i);
 });
