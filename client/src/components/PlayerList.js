59efc751b999f2d7653e86c8b996e915affd8ed2
eralpkor <eralp.ny@gmail.com>
2019-09-06 11:17:05 -0400

    added playerlist class component

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const playerURL = 'http://localhost:5000/api/players';
+
 
 class PlayerList extends Component {
+  componentDidMount() {
+    axios.get(playerURL)
+    .then(response => {
+      console.log(response.data[0])
+    })
+    .catch(err => console.log(err))
+  }
 
   render() {
     return <h1>Hello Players</h1>
